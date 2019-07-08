7ba0876d23366d1819268c937912d3257bb49419
Jigar Gosar <jigar.gosar@gmail.com>
2019-07-08 21:42:57 +0530

    typestyle

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
-// import 'tachyons'
+import 'tachyons'
 // import './main.css'
-import { setupPage, normalize, vertical } from 'csstips'
+import { setupPage, normalize } from 'csstips'
 normalize()
 setupPage('#root')
 
 import { classes } from 'typestyle'
-import { pa3, frow2 } from './fcss'
+import { frow2 } from './fcss'
 
 const App: React.FC = function() {
   return (
-    <div className={classes(pa3, vertical)}>
+    <div className="pa3">
       <div>Hello World</div>
       <div className={classes(frow2)}>
         <div>a</div>
