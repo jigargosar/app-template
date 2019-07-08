b25633f6d4535fabb1283f440dc15449a8261afe
Jigar Gosar <jigar.gosar@gmail.com>
2019-07-08 17:53:04 +0530

    app

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,4 +2,14 @@ import React from 'react'
 import { render } from 'react-dom'
 import 'tachyons'
 
-render(<div>Hello</div>, document.getElementById('root'))
+const App: React.FC = function() {
+  return (
+    <div>
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    </div>
+  )
+}
+
+render(<App />, document.getElementById('root'))
