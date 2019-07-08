c9085db07d23e4a02802fb27a823b237c6e59dfb
Jigar Gosar <jigar.gosar@gmail.com>
2019-07-08 21:46:13 +0530

    typestyle

--- src/app/fcss.ts
+++ src/app/fcss.ts
@@ -2,7 +2,12 @@ import { rem } from 'csx'
 
 import { style } from 'typestyle'
 
-import { horizontal, horizontallySpaced } from 'csstips'
+import {
+  horizontal,
+  horizontallySpaced,
+  vertical,
+  verticallySpaced,
+} from 'csstips'
 
 const space1 = rem(0.25)
 const space2 = rem(0.5)
@@ -14,3 +19,8 @@ const frow = (margin: string | number) =>
 export const frow1 = frow(space1)
 export const frow2 = frow(space2)
 export const frow3 = frow(space3)
+
+const fcol = (margin: string | number) =>
+  style(vertical, verticallySpaced(margin))
+
+export const fcol3 = fcol(space3)
